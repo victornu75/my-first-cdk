c4aee5464f06e8e66e017a62a389212d53b532b3
victornu75 <113864213+victornu75@users.noreply.github.com>
2024-04-13 17:43:34 -0600

    Update my-first-cdk-app.test.ts

--- test/my-first-cdk-app.test.ts
+++ test/my-first-cdk-app.test.ts
@@ -4,7 +4,7 @@ describe('My First CDK App Tests', () => {
 
     it('returns name and test', async () => {
         const output = await handler({ name: 'Victor' });
-        expect(output).toStrictEqual('Good Job Victor')
+        expect(output).toStrictEqual('Good Job Victor!')
     })
 
     it('returns no name and test', async () => {
