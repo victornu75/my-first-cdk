b07d3d32189fa4546a8d7a22eaea3284ce98652f
victornu75 <113864213+victornu75@users.noreply.github.com>
2024-04-13 17:06:48 -0600

    adding ci cd and test

--- test/my-first-cdk.test.ts
+++ test/my-first-cdk.test.ts
@@ -1,17 +1,16 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as MyFirstCdk from '../lib/my-first-cdk-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as MyFirstCdk from '../lib/my-first-cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/my-first-cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new MyFirstCdk.MyFirstCdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('My First Stack Created', () => {
+  const app = new cdk.App();
+  const stack = new MyFirstCdk.MyFirstCdkStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    FunctionName: 'first-cdk-lambda',
+    Runtime: 'nodejs16.x',
+    Handler: 'index.handler',
+    MemorySize: 128
+  });
 });
